1d2110d30784464fe792e306550c1f30973beaf3
Stephanie Asmar <sasmar8@gmail.com>
2017-08-10 12:03:31 -0400

    added scripts to json; writes test for index.js file

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 require('dotenv').config()
+const pg = require('pg-promise')();
+const dbConfig = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_NAME
+};
 
-// var db = require('db')
-// db.connect({
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USER,
-// });
+const db = pg(dbConfig);
+
+class Customer {
+    constructor(name, email, addr, password) {
+        this.name = name;
+        this.email = email;
+        this.address = addr;
+        this.password = password;
+    }
+    save() {
+        return db.query(`
+        insert into customers
+        (name, email, address, password)
+        values
+        ('${this.name}', '${this.email}', '${this.address}', '${this.password}');
+        `);
+    }
+    get(id) {
+        return db.one(`
+            select name, email, address from customers where customer_id=${id};
+        `).then((result) => {
+            this.name = result.name;
+            this.email = result.email;
+            this.address = result.address;
+            return result;
+        })
+    }
+}
+
+module.exports = Customer;
--- test/test-customer-model.js
+++ test/test-customer-model.js
@@ -1,7 +1,27 @@
-const expect = chai.expect;
+const expect = require('chai').expect;
+const Customer = require('../index');
 
 describe('Customers', () => {
-    it('should be able to save to the database', () => {
-        expect(1 + 1).to.equal(2);
+    it('should be able to save to the database', (done) => {
+        
+        let myCustomer = new Customer('me', 'me@me.com', '123 me street', 'm3m3m3');
+        myCustomer
+            .save()
+            .then((result) => {
+
+                done();
+            });      
+    });
+    it('should be able to get a customer from the database', (done) => {
+        let myCustomer = new Customer();
+        myCustomer
+            .get(1)
+            .then((result) => {
+                console.log(myCustomer.name);
+                console.log(myCustomer.email);
+                console.log(myCustomer.address);
+                console.log(myCustomer.password);
+                done();
+            });
     });
 });
