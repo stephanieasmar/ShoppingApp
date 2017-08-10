52ce69896ba0f752482437b67a94599c8c04b2f7
Stephanie Asmar <sasmar8@gmail.com>
2017-08-10 13:36:19 -0400

    created db file, moved db info from index.js to new db.js file

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-require('dotenv').config()
-const pg = require('pg-promise')();
-const dbConfig = {
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    database: process.env.DB_NAME
-};
-
-const db = pg(dbConfig);
+const db = require('./db');
 
 class Customer {
     constructor(name, email, addr, password) {
