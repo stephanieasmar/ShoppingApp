0536783b03fb3197a2ceab078e518a20550e609a
Stephanie Asmar <sasmar8@gmail.com>
2017-08-10 11:02:22 -0400

    adds schema for models; adds test and class file

---
+++ schema.sql
@@ -0,0 +1,23 @@
+create table customers (
+    customer_id serial primary key,
+    name varchar (200) not null,
+    email varchar (200) not null,
+    address varchar (300) not null,
+    password varchar (50) not null
+);
+
+create table items (
+    item_id serial primary key,
+    name varchar (200) not null,
+    destription text not null,
+    price numeric not null,
+    quantity numeric not null
+);
+
+-- for later: add reviews, add ratings
+
+create table orders (
+    order_id serial primary key,
+    item_id integer references items,
+    customer_id integer references customers
+);
